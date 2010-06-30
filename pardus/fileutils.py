e500b019246d059bb643f19f9cefdc771953b724
Ozan Çağlayan <ozan@pardus.org.tr>
2010-06-30 17:49:16 +0000

    Update copyright dates

--- pardus/fileutils.py
+++ pardus/fileutils.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-# Copyright (C) 2006, TUBITAK/UEKAE
+# Copyright (C) 2006-2010 TUBITAK/UEKAE
 #
 # This program is free software; you can redistribute it and/or modify it under
 # the terms of the GNU General Public License as published by the Free
@@ -21,18 +21,18 @@ class FileLock:
     def __init__(self, filename):
         self.filename = filename
         self.fd = None
-    
+
     def lock(self, shared=False, timeout=-1):
         _type = fcntl.LOCK_EX
         if shared:
             _type = fcntl.LOCK_SH
         if timeout != -1:
             _type |= fcntl.LOCK_NB
-        
+
         self.fd = os.open(self.filename, os.O_WRONLY | os.O_CREAT, 0600)
         if self.fd == -1:
             raise IOError, "Cannot create lock file"
-        
+
         while True:
             try:
                 fcntl.flock(self.fd, _type)
@@ -43,7 +43,6 @@ class FileLock:
                     timeout -= 0.2
                 else:
                     raise
-    
+
     def unlock(self):
         fcntl.flock(self.fd, fcntl.LOCK_UN)
-
